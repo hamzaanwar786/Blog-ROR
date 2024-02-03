7391f15166d7c530e4cab251b48f15075959567a
Hamza <hafizhamza660@gmail.com>
2024-02-03 19:40:59 +0500

    Adding seed and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do |x|
+  Post.create(title: "POST #{x+1}",body: "This is post #{x+1}")
+end
